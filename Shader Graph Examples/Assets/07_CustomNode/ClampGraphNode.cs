53c54e3496844a4593787cdc2693a8b4725a501c
Arturo Nereu <hdgam3r@gmail.com>
2019-04-16 13:53:09 -0500

    Updated to 2019.1f2

--- Shader Graph Examples/Assets/07_CustomNode/ClampGraphNode.cs
+++ Shader Graph Examples/Assets/07_CustomNode/ClampGraphNode.cs
@@ -1,35 +1,35 @@
-﻿using UnityEngine;
-using UnityEditor.ShaderGraph;
-using System.Reflection;
+﻿//using UnityEngine;
+//using UnityEditor.ShaderGraph;
+//using System.Reflection;
 
-[Title("Custom", "Clamp Node")]
-public class ClampGraphNode : CodeFunctionNode
-{
-    public ClampGraphNode()
-    {
-        name = "Clamp Node";
-    }
+//[Title("Custom", "Clamp Node")]
+//public class ClampGraphNode : CodeFunctionNode
+//{
+//    public ClampGraphNode()
+//    {
+//        name = "Clamp Node";
+//    }
 
-    protected override MethodInfo GetFunctionToConvert()
-    {
-        return GetType().GetMethod("ClampFunction", BindingFlags.Static | BindingFlags.NonPublic);
-    }
+//    protected override MethodInfo GetFunctionToConvert()
+//    {
+//        return GetType().GetMethod("ClampFunction", BindingFlags.Static | BindingFlags.NonPublic);
+//    }
 
-    static string ClampFunction([Slot(0, Binding.None)] DynamicDimensionVector Value, [Slot(1, Binding.None)] DynamicDimensionVector Min, [Slot(2, Binding.None)] DynamicDimensionVector Max, [Slot(3, Binding.None)]out DynamicDimensionVector Out)
-    {
-        return @"
-{
-            Out = Value;
-            if(Value < Min)
-            {
-                Out = Min;
-            }
+//    static string ClampFunction([Slot(0, Binding.None)] DynamicDimensionVector Value, [Slot(1, Binding.None)] DynamicDimensionVector Min, [Slot(2, Binding.None)] DynamicDimensionVector Max, [Slot(3, Binding.None)]out DynamicDimensionVector Out)
+//    {
+//        return @"
+//{
+//            Out = Value;
+//            if(Value < Min)
+//            {
+//                Out = Min;
+//            }
             
-            if(Value > Max)
-            {
-                Out = Max;
-            }
-}
-        ";
-    }
-}
+//            if(Value > Max)
+//            {
+//                Out = Max;
+//            }
+//}
+//        ";
+//    }
+//}
